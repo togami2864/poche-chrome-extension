9c580048bd7c109b3009ec1b3f01878cfd5c3a04
togami2864 <tuabobo123@gmail.com>
2021-10-03 15:25:22 +0900

    faet: add background

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,40 @@
 <script lang="ts">
-	export let name: string;
+  let title: string = "togami2864 (Yoshiaki Togami)";
+  let url: string = "https://github.com/togami2864";
+  const queryOptions = {
+    active: true,
+    currentWindow: true,
+  };
+  // chrome.tabs.query(queryOptions, (tabs) => {
+  //   const currentTab = tabs[0];
+  //   title = currentTab.title;
+  //   url = currentTab.url;
+  // });
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <input value={title} />
+  <h2>{url}</h2>
+  <textarea placeholder="メモ" />
+  <select>
+    <option>public</option>
+    <option>private</option>
+  </select>
+  <button>Add</button>
+  <button>Discard</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 480px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
